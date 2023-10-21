fc8216c1be861ce898df9b81e581b071f7be088c
cyberwarrior32 <148263379+cyberwarrior32@users.noreply.github.com>
2023-10-21 11:44:05 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all code as newer version 
+
+// alert(3+3) WE ARE USING NODE JS ,NOT BROWSER
+
+console.log(3+3)
+
+console.log("Hasan")
+
+
+let name = "Hasan"
+let age = 19
+let IsLogIn = false
+
+// number => 2 to power 53 almost
+// bright
+// boolean => True/False
+// string => unique
+// symbol => 
+// null => standalone value 
+// undefined => ""
+
+
+//object
+
+console.log(typeof null)
