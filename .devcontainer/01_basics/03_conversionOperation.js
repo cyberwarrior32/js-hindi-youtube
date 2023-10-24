e84a1451357723c3b22be28bd40ec8849b87af5f
cyberwarrior32 <148263379+cyberwarrior32@users.noreply.github.com>
2023-10-24 05:28:26 +0000

    conversion of dataTypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score = false
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+// "33" => 33
+// 33abc => NaN
+// true => 1 ; False => 0
+
+let isLoggedIn = "Hasan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=>True ; 0=>False
+// "" =>False
+// "Hasan"=>True
+
+let someNumber= 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
